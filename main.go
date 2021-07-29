11d87bd2e292df1c3a2ddecb2de61315e75ed7db
Todd W Crone <twcrone@gmail.com>
2021-07-29 15:36:21 -0400

    Add actual logic for adding extensions to files that don't already have one

--- main.go
+++ main.go
@@ -4,13 +4,31 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"strings"
 )
 
 func main() {
+	args := os.Args
+	var ext string
+	if len(args) == 2 {
+		ext = args[1]
+		fmt.Println("Adding extension [" + ext + "]")
+	} else {
+		log.Fatal("Usage: addext [extension]")
+	}
 	files, err := ioutil.ReadDir("./")
 	check(err)
 	for _, f := range files {
-		fmt.Println(f.Name())
+		currentFilename := f.Name()
+		if strings.Contains(currentFilename, ".") == false {
+			newFilename := currentFilename + "." + ext
+			fmt.Printf("Renaming %q to %q\n", currentFilename, newFilename)
+			err := os.Rename(currentFilename, newFilename)
+			check(err)
+		} else {
+			fmt.Printf("Skipping %q\n", currentFilename)
+		}
 	}
 }
 
